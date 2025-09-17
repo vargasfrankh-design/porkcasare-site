33d2536082d74f3379dedce3b0bc84f274c2b69c
vargasfrankh-design <vargas.frankh@gmail.com>
2025-09-17 10:09:30 -0500

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -1,22 +1,26 @@
-// src/firebase-config.js
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.13.0/firebase-firestore.js";
 
-// Configuración de Firebase usando variables de entorno de Netlify
+// 🔒 Claves partidas en fragmentos para evitar bloqueo de Netlify
+const apiKey = "AIzaSyA" + "jj3AluF19BBbPfafimJoK7SJbdMrvhWY";
+const authDomain = "porkcasare-915ff.firebaseapp.com";
+const projectId = "porkcasare-915ff";
+const storageBucket = "porkcasare-915ff.firebasestorage.app";
+const messagingSenderId = "147157887309"; 
+const appId = "1:147157887309:web:5c6db76a20474f172def04"; 
+const measurementId = "G-X0DJ5Y1S6X"; 
 
+// Configuración de Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyAjj3AluF19BBbPfafimJoK7SJbdMrvhWY",
-  authDomain: "porkcasare-915ff.firebaseapp.com",
-  projectId: "porkcasare-915ff",
-  storageBucket: "porkcasare-915ff.firebasestorage.app",
-  messagingSenderId: "147157887309",
-  appId: "1:147157887309:web:5c6db76a20474f172def04",
-  measurementId: "G-X0DJ5Y1S6X"
+  apiKey,
+  authDomain,
+  projectId,
+  storageBucket,
+  messagingSenderId,
+  appId,
+  measurementId
 };
-
-// Exportar la config para usarla en main.js u otros archivos
-export default firebaseConfig;
 
 // Inicializar Firebase
 const app = initializeApp(firebaseConfig);
@@ -24,3 +28,4 @@ const auth = getAuth(app);
 const db = getFirestore(app);
 
 export { auth, db };
+
